9a37f49745db32902e5d22ffa2b738c9ffd7fda7
Andrzej Ojczenasz <codelaborer@gmail.com>
2017-10-31 14:18:52 +0100

    parhelium-tech/final-draft-studio/#643 - MFA guardian

---
+++ rules/Multifactor-Guardian-Do-Not-Rename.js
@@ -0,0 +1,25 @@
+function (user, context, callback) {
+
+  if (!user.app_metadata || !user.app_metadata.authorization ||
+    !Array.isArray(user.app_metadata.authorization.groups)) {
+    return callback(null, user, context);
+  }
+
+  var groups = user.app_metadata.authorization.groups;
+
+  // check if user belong to any group acitve in Authorization Extension
+  var needsMFA = groups && groups.length > 0;
+
+  if (needsMFA){
+    context.multifactor = {
+      // required
+      provider: 'guardian', //required
+
+      // optional, defaults to true. Set to false to force Guardian authentication every time.
+      // See https://auth0.com/docs/multifactor-authentication/custom#change-the-frequency-of-authentication-requests for details
+      allowRememberBrowser: false
+    };
+  }
+
+  callback(null, user, context);
+}
