deb2482781ae8e1468f5f02b677017d6f9558c8c
Andrzej Ojczenasz <codelaborer@gmail.com>
2017-10-31 22:26:33 +0100

    parhelium-tech/final-draft-studio/#643 - MFA - added disabling MFA for specified clients - iOS etc

--- rules/Multifactor-Guardian-Do-Not-Rename.js
+++ rules/Multifactor-Guardian-Do-Not-Rename.js
@@ -1,24 +1,29 @@
 function (user, context, callback) {
 
-  if (!user.app_metadata || !user.app_metadata.authorization ||
-    !Array.isArray(user.app_metadata.authorization.groups)) {
-    return callback(null, user, context);
-  }
+  var CLIENTS_WITHOUT_MFA = [''];
+  // run only if client has disabled mfa
 
-  var groups = user.app_metadata.authorization.groups;
+  if (CLIENTS_WITHOUT_MFA.indexOf(context.clientID) === -1) {
+    if (!user.app_metadata || !user.app_metadata.authorization ||
+      !Array.isArray(user.app_metadata.authorization.groups)) {
+      return callback(null, user, context);
+    }
 
-  // check if user belong to any group acitve in Authorization Extension
-  var needsMFA = groups && groups.length > 0;
+    var groups = user.app_metadata.authorization.groups;
 
-  if (needsMFA){
-    context.multifactor = {
-      // required
-      provider: 'guardian', //required
+    // check if user belong to any group acitve in Authorization Extension
+    var needsMFA = groups && groups.length > 0;
 
-      // optional, defaults to true. Set to false to force Guardian authentication every time.
-      // See https://auth0.com/docs/multifactor-authentication/custom#change-the-frequency-of-authentication-requests for details
-      allowRememberBrowser: false
-    };
+    if (needsMFA){
+      context.multifactor = {
+        // required
+        provider: 'guardian', //required
+
+        // optional, defaults to true. Set to false to force Guardian authentication every time.
+        // See https://auth0.com/docs/multifactor-authentication/custom#change-the-frequency-of-authentication-requests for details
+        allowRememberBrowser: false
+      };
+    }
   }
 
   callback(null, user, context);
